106a882645e48fa4fb59a50ff4e4acf23972846e
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2026-02-28 21:44:58 -0500

    upload

---
+++ data.js
@@ -0,0 +1,274 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1-thin-thn-truyn-tin-cho-c-b-chu-thai",
+      "name": "一、聖母領報",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1948400729514894,
+          "pitch": 0.05525989617790472,
+          "rotation": 0,
+          "target": "1-2-c-b-i-ving-b-thnh-isave"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.160054435162591,
+          "pitch": -0.28169234161257073,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">聖母領報</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">到了第六個月，天使加俾額爾奉天主差遣，往加里肋亞一座名叫納匝肋的城去，到一位童貞女那裏，她已與達味家族中的一個名叫若瑟的男子訂了婚，童貞女的名字叫瑪利亞。</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">天使進去向她說：「萬福！充滿恩寵者，上主與你同在！」【在女人中你是蒙祝福的。】她卻因這話驚惶不安，便思慮這樣的請安有什麼意思。</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">天使對她說：「瑪利亞，不要害怕，因為你在天主前獲得了寵幸。看，你將懷孕生子，並要給他起名叫耶穌。他將是偉大的，並被稱為至高者的兒子，上主天主要把他祖先達味的御座賜給他。他要為王統治雅各伯家，直到永遠；他的王權沒有終結。」</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">瑪利亞便向天使說：「這事怎能成就？因為我不認識男人。」</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">天使答覆她說：「聖神要臨於你，至高者的能力要庇廕你，因此，那要誕生的聖者，將稱為天主的兒子。且看，你的親戚依撒伯爾，她雖在老年，卻懷了男胎，本月已六個月了，她原是素稱不生育的，因為在天主前沒有不能的事。」</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">瑪利亞說：「看！上主的婢女，願照你的話成就於我罷！」天使便離開她去了。<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">(路加福音 1:26-38)</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-2-c-b-i-ving-b-thnh-isave",
+      "name": "二、聖母往見聖婦依撒伯爾",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.08240088306867044,
+          "pitch": -0.45941287007660847,
+          "rotation": 0,
+          "target": "2-3-c-b-sinh-c-cha-gisu-ni-hang-"
+        },
+        {
+          "yaw": -2.027630697812615,
+          "pitch": -0.37836001166965794,
+          "rotation": 0,
+          "target": "0-1-thin-thn-truyn-tin-cho-c-b-chu-thai"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.12524373470416705,
+          "pitch": -0.6640726414363716,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">聖母往見聖婦依撒伯爾</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">瑪利亞就在那幾日起身，急速往山區去，到了猶大的一座城。她進了匝加利亞的家，就給依撒伯爾請安。依撒伯爾一聽到瑪利亞請安，胎兒就在她的腹中歡躍。依撒伯爾遂充滿了聖神，大聲呼喊說：「在女人中你是蒙祝福的，你的胎兒也是蒙祝福的。吾主的母親駕臨我這裏，這是我那裏得來的呢？看，你請安的聲音一入我耳，胎兒就在我腹中歡喜踴躍。那信了由上主傳於她的話必要完成的，是有福的。」<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">(路加福音 1:39-45)</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-3-c-b-sinh-c-cha-gisu-ni-hang-",
+      "name": "三、耶穌基督降生",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.11714081544133315,
+          "pitch": -0.15866660765025742,
+          "rotation": 0,
+          "target": "3-4-c-b-dng-c-cha-gisu-trong-n-thnh"
+        },
+        {
+          "yaw": 2.87401091767512,
+          "pitch": -0.2509765799379853,
+          "rotation": 0,
+          "target": "1-2-c-b-i-ving-b-thnh-isave"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.16032498761056857,
+          "pitch": -0.4172683295906481,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">耶穌基督降生</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">那時凱撒奧古斯都出了一道上諭，叫天下的人都要登記：這是在季黎諾作敘利亞總督時，初次行的登記。於是，眾人各去本城登記。若瑟因為是達味家族的人，也從加里肋亞納匝肋城，上猶大名叫白冷的達味城去，好同自己已懷孕的聘妻瑪利亞去登記。他們在那裏的時候，她分娩的日期滿了，便生了她的頭胎男兒，用襁褓裹起，放在馬槽裏，因為在客棧中為他們沒有地方。</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">在那地區有些牧人露宿，守夜看守羊群。有上主的一個天使站在他們身邊，上主的光耀環照着他們，他們便非常害怕。天使向他們說：「不要害怕！看！我給你們報告一個為全民族的大喜訊：今天在達味城中，為你們誕生了一位救世者，他是主默西亞。這是給你們的記號：你們將要看見一個嬰兒，裹着襁褓，躺在馬槽裏。」忽有一大隊天軍，同那天使一起讚頌天主說：</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">「天主受享光榮於高天，主愛的人在世享平安！」</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">眾天使離開他們往天上去了以後，牧人們就彼此說：「我們且往白冷去，看看上主報告給我們所發生的事。」他們急忙去了，找到了瑪利亞和若瑟，並那躺在馬槽中的嬰兒。他們看見以後，就把天使對他們論這小孩所說的事，傳揚開了，凡聽見的人都驚訝牧人向他們所說的事。瑪利亞卻把這一切事默存在自己心中，反覆思想。牧人們為了他們所聽見和看見的一切，正如天使向他們說的一樣，就光榮讚美天主回去了。<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">(路加福音 2:1-20)</span></p>"
+        }
+      ]
+    },
+    {
+      "id": "3-4-c-b-dng-c-cha-gisu-trong-n-thnh",
+      "name": "四、聖母獻耶穌於聖殿",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.04448042282405673,
+          "pitch": -0.3609151715076102,
+          "rotation": 0,
+          "target": "4-5-c-b-tm-c-c-cha-gisu-trong-n-thnh"
+        },
+        {
+          "yaw": -2.998004314665394,
+          "pitch": -0.1812815647630721,
+          "rotation": 0,
+          "target": "2-3-c-b-sinh-c-cha-gisu-ni-hang-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.004950021509621649,
+          "pitch": -0.5050909207814644,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">聖母獻耶穌於聖殿</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">按梅瑟的法律，一滿了他們取潔的日期，他們便帶着孩子上耶路撒冷去獻給上主，就如上主的法律上所記載的：「凡開胎首生的男性，應祝聖於上主。」並該照上主法律上所吩咐的，獻上祭物：一對斑鳩或兩隻鶵鴿。那時，在耶路撒冷有一個人，名叫西默盎。這人正義虔誠，期待着以色列的安慰，而且聖神也在他身上。他曾蒙聖神啟示：自己在未看見上主的受傅者以前，決見不到死亡。他因聖神的感動，進了聖殿；那時，抱着嬰孩耶穌的父母正進來，要按着法律的慣例為他行禮。西默盎就雙臂接過他來，讚美天主說：</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">「主啊！現在可照你的話，放你的僕人平安去了！因為我親眼看見了你的救援，即你在萬民之前早準備好的：為作啟示異邦的光明，你百姓以色列的榮耀。」</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">他的父親和母親就驚異他關於耶穌所說的這些話。西默盎祝福了他們，又向他的母親瑪利亞說：「看，這孩子已被立定，為使以色列中許多人跌倒和復起，並成為反對的記號──至於你，要有一把利劍剌透你的心靈──為叫許多人心中的思念顯露出來。」</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">又有一位女先知亞納，是阿協爾支派法奴耳的女兒，已上了年紀。她出閣後，與丈夫同居了七年，以後就守寡，直到八十四歲。她齋戒祈禱，晝夜事奉天主，總不離開聖殿。正在那時刻，她也前來稱謝天主，並向一切希望耶路撒冷得救贖的人，講論這孩子。<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">(路加福音 2:22-38)</span></p>"
+        }
+      ]
+    },
+    {
+      "id": "4-5-c-b-tm-c-c-cha-gisu-trong-n-thnh",
+      "name": "五、耶穌十二齡講道",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.224982886550988,
+          "pitch": 0.01823191313473771,
+          "rotation": 0,
+          "target": "3-4-c-b-dng-c-cha-gisu-trong-n-thnh"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.15803943551415855,
+          "pitch": -0.41004943600042054,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">耶穌十二齡講道</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">他的父母每年逾越節往耶路撒冷去。他到了十二歲時，他們又照節日的慣例上去了。過完了節日，他們回去的時候，孩童耶穌卻留在耶路撒冷，他的父母並未發覺。他們只以為他在同行的人中間，遂走了一天的路程；以後，就在親戚和相識的人中尋找他。既找不着，便折回耶路撒冷找他。</span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">過了三天，纔在聖殿裏找到了他。他正坐在經師中，聆聽他們，也詢問他們。凡聽見他的人，對他的智慧和對答，都驚奇不止。他們一看見他，便大為驚異，他的母親就向他說：「孩子，為什麼你這樣對待我們？看，你的父親和我，一直痛苦的找你。」耶穌對他們說：「你們為什麼尋找我？你們不知道我必須在我父親那裏嗎？」但是，他們不明白他對他們所說的話。<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 14pt; line-height: 115%;\">(路加福音 2:41-50)</span></p>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
